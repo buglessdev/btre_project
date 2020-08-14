e7abfafd48856635a980c5280ffe721d5a9cbc34
excelligence <excelligence.systems@gmail.com>
2020-08-14 07:09:58 +0530

    lec 46 search form filtering

--- listings/views.py
+++ listings/views.py
@@ -28,9 +28,51 @@ def listing(request, listing_id):
 
 
 def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(
+                description__iexact=keywords)
+
+    # City
+
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(
+                city=city)
+
+    # State
+
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(
+                state_iexact=state)
+
+    # Bedrooms
+
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(
+                bedrooms__lte=bedrooms)
+
+    # Price
+
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(
+                price__lte=price)
+
     context = {
         'state_choices': state_choices,
         'bedroom_choices': bedroom_choices,
-        'price_choices': price_choices
+        'price_choices': price_choices,
+        'listings': queryset_list
     }
     return render(request, 'listings/search.html', context)
