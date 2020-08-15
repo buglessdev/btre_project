c1d3672a830f47fafd45eef84454e46cd0b51703
excelligence <excelligence.systems@gmail.com>
2020-08-15 07:03:33 +0530

    lec 50 message alerts

--- btre/settings.py
+++ btre/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+from django.contrib.messages import constants as messages
 import os
 from decouple import config
 
@@ -136,3 +137,9 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+
+}
